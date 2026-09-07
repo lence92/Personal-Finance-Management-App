04ec6b3d89886ff2a314be854f7f48e80d39030f
Lenche Petkovska <lenche.petkovska.icloud.com>
2026-09-07 21:31:12 +0200

    PFMA-45 update to angular 22

--- src/app/features/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/features/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'app-dashboard-page',
     imports: [],
     templateUrl: './dashboard-page.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrl: './dashboard-page.component.scss'
 })
 export class DashboardPageComponent {
